2bb4d3fd0ab4b457a0e24ea0f219806917b698bc
FatemeZamanian <fatemehzamanian.1999@gmail.com>
2021-06-19 02:47:17 +0430

    i2

--- boards/board.py
+++
@@ -1,9 +0,0 @@
-import cv2
-org=cv2.imread('board - origin.bmp')
-tst=cv2.flip(cv2.imread('board - test.bmp'),1)
-org=cv2.cvtColor(org,cv2.COLOR_RGB2GRAY)
-tst=cv2.cvtColor(tst,cv2.COLOR_RGB2GRAY)
-res = cv2.subtract(tst, org)*3
-res=cv2.resize(res,(500,500))
-cv2.imshow('board',res)
-cv2.waitKey()
